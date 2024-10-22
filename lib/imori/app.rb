4b7bb471ace76ee1bf34a890ca6493aa0c83f6dc
takanobu maekawa <tmaekawa@jcom.home.ne.jp>
2024-10-23 00:07:21 +0900

    0.1.0 enhance commands in console and add project generation sub command

--- lib/imori/app.rb
+++ lib/imori/app.rb
@@ -2,6 +2,19 @@ require 'gli'
 require 'irb'
 require 'imori/config'
 require 'imori/utils/irb'
+require 'imori/generate/project'
+require 'i18n'
+
+#
+# i18n settings
+#
+I18n.load_path += Imori.config.i18n.load_path
+I18n.default_locale = :en
+I18n.locale = /jp|JP|ja|ja_JP/.match?(ENV['LANG']) ? :ja : :en
+
+def t(indicator)
+  I18n.t(indicator)
+end
 
 module Imori
   class App
@@ -9,16 +22,42 @@ module Imori
 
     using IRBUtils
 
-    program_desc 'A Ruby-styled Salesforce console utility'
+    program_desc t('cli.desc')
 
-    desc 'Username or alias of the target org'
-    flag [:o, 'target-org'], default_value: nil
+    desc t('cli.target_org')
+    flag [:o, 'target-org'], default_value: nil, arg_name: 'org'
 
-    desc 'start irb session'
+    desc t('cli.irb.desc')
     command :irb do |c|
+      c.desc desc t('cli.target_org')
+      c.flag [:o, 'target-org'], default_value: nil, arg_name: 'org'
+
       c.action do |global_options, options, args|
-        Imori.config.cli_options[:"target-org"] = global_options['target-org']
+        Imori.config.cli_options[:"target-org"] = global_options['target-org'] || options['target-org']
         IRB.start(__FILE__, ['-r', 'imori/irb', '--noscript', global_options['target-org']])
+      end
+    end
+
+    desc t('cli.generate.desc')
+    command [:generate, :g] do |c|
+      c.desc desc t('cli.generate.project.desc')
+      c.arg_name 'project_name'
+      c.command :project do |prj|
+        prj.desc t('cli.generate.project.target_org')
+        prj.flag [:o, 'target-org'], default_value: nil, arg_name: 'org'
+
+        prj.desc t('cli.generate.project.manifest')
+        prj.switch [:m, :manifest], negatable: false
+
+        prj.desc t('cli.generate.project.open_editor')
+        prj.switch [:e, 'open-editor'], negatable: false
+
+        prj.desc t('cli.generate.project.retrieve')
+        prj.switch [:r, 'retrieve'], negatable: false
+
+        prj.action do |_, options, args|
+          Imori::Project.generate(args.first, **options)
+        end
       end
     end
 
--- lib/imori/irb/command/query.rb
+++ lib/imori/irb/command/query.rb
@@ -1,9 +1,21 @@
+require 'yamori/query_condition'
+
 class Query < IRB::Command::Base
   category "Imori"
   description t('query.description')
   help_message TTY::Markdown.parse t('query.help')
 
-  def execute(soql)
+  def execute(arg)
+    soql =
+      if /\ASELECT/.match?(arg.strip.upcase)
+        arg
+      else
+        object = eval(arg)
+        object.to_soql if object&.instance_of? Yamori::QueryMethods::QueryCondition
+      end
+
+    return if soql.nil?
+
     puts sf.data.query(soql, format: :human, target_org: ::Imori.config.connection.alias)
   end
 end
