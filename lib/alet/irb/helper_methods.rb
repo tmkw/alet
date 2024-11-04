1d4453474617ae0e10c802ad0aa64fd9e7538e4f
tmkw <tmaekawa@jcom.home.ne.jp>
2024-11-04 19:21:11 +0900

    0.2.0/feat: add org command (replacing conn helper method) (#101)

--- lib/alet/irb/helper_methods.rb
+++ lib/alet/irb/helper_methods.rb
@@ -1,24 +1,5 @@
 require 'irb/helper_method/base'
 
-class CurrentConnection < IRB::HelperMethod::Base
-  description t('connection.description')
-
-  def execute
-    info = Alet.config.connection
-    table =
-      TTY::Table.new(rows: [
-        [:id, info.id],
-        [:alias, info.alias],
-        [:user_name, info.user_name],
-        [:status, info.status],
-        [:instance_url, info.instance_url],
-        [:api_version, info.api_version],
-        [:access_token, info.access_token]
-      ])
-    puts table.render :unicode
-  end
-end
-
 class Apex < IRB::HelperMethod::Base
   description t('apex.description')
 
