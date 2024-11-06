6a5e5fa1919131b62d7c8d7502dbd976b1419437
tmkw <tmaekawa@jcom.home.ne.jp>
2024-11-07 00:02:58 +0900

    0.2.0/feat: desc command (#105)

--- lib/alet.rb
+++ lib/alet.rb
@@ -16,4 +16,8 @@ module Alet
   def self.describe_global
     @describe_global ||= rest_client.describe_global
   end
+
+  def self.describe(object_type)
+    rest_client.describe object_type
+  end
 end
