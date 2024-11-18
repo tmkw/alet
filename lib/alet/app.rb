9e12d06b64a3b66f4782aa205436bdccec5035de
takanobu maekawa <tmaekawa@jcom.home.ne.jp>
2024-11-18 17:39:31 +0900

    0.2.0/feat: project update

--- lib/alet/app.rb
+++ lib/alet/app.rb
@@ -29,11 +29,9 @@ module Alet
     desc t('cli.target_org')
     flag [:o, 'target-org'], default_value: nil, arg_name: 'org'
 
-
     desc t('cli.irb.desc')
     command :irb do |c|
       c.desc desc t('cli.target_org')
-      c.flag [:o, 'target-org'], default_value: nil, arg_name: 'org'
 
       c.example "alet", desc: t('cli.irb.example.default')
       c.example "alet -o org", desc: t('cli.irb.example.target_org')
@@ -45,29 +43,34 @@ module Alet
     end
 
     desc t('cli.project.desc')
-    command [:project, :p] do |c|
-      c.desc desc t('cli.project.generate.desc')
-      c.arg_name 'project_name'
-      c.command [:generate, :g] do |prj|
-        prj.desc t('cli.project.generate.target_org')
-        prj.flag [:o, 'target-org'], default_value: nil, arg_name: 'org'
+    command [:project, :p] do |prj|
+      prj.desc desc t('cli.project.generate.desc')
+      prj.arg_name 'project_name'
+      prj.command [:generate, :g] do |gen|
+        gen.desc t('cli.project.generate.manifest')
+        gen.switch [:m, :manifest], negatable: false
 
-        prj.desc t('cli.project.generate.manifest')
-        prj.switch [:m, :manifest], negatable: false
+        gen.desc t('cli.project.generate.open_editor')
+        gen.switch [:e, 'editor-open'], negatable: false
 
-        prj.desc t('cli.project.generate.open_editor')
-        prj.switch [:e, 'editor-open'], negatable: false
+        gen.desc t('cli.project.generate.retrieve')
+        gen.switch [:r, 'retrieve'], negatable: false
 
-        prj.desc t('cli.project.generate.retrieve')
-        prj.switch [:r, 'retrieve'], negatable: false
+        gen.example "alet generate project MyProject", desc: t('cli.project.generate.example.default')
+        gen.example "alet generate project MyProject -m", desc: t('cli.project.generate.example.manifest')
+        gen.example "alet -o org generate project MyProject -m", desc: t('cli.project.generate.example.from_org')
+        gen.example "alet -o org generate project MyProject -mr", desc:t('cli.project.generate.example.retrieve')
 
-        prj.example "alet generate project MyProject", desc: t('cli.project.generate.example.default')
-        prj.example "alet generate project MyProject -m", desc: t('cli.project.generate.example.manifest')
-        prj.example "alet generate project MyProject -m -o org", desc: t('cli.project.generate.example.from_org')
-        prj.example "alet generate project MyProject -mr -o org", desc:t('cli.project.generate.example.retrieve')
-
-        prj.action do |_, options, args|
+        gen.action do |global_options, _options, args|
+          options = global_options.merge _options
           Alet::Project.generate(args.first, **options)
+        end
+      end
+
+      prj.command [:update, :u] do |update|
+        update.action do |global_options, _options, args|
+          options = global_options.merge _options
+          Alet::Project.update(args.first, **options)
         end
       end
     end
--- lib/alet/generate/project.rb
+++ lib/alet/generate/project.rb
@@ -1,4 +1,5 @@
 require 'sf_cli'
+require 'fileutils'
 
 module Alet
   module Project
@@ -8,15 +9,33 @@ module Alet
       retrieve_source = params[:retrieve]
       editor          = params[:"editor-open"]
 
-      output = sf.project.generate project_name, manifest: true, raw_output: true
-      puts output
+      return if project_name.nil?
+
+      puts sf.project.generate project_name, manifest: true, raw_output: true
 
       Dir.chdir project_name
 
-      output = sf.project.generate_manifest from_org: target_org, output_dir: 'manifest', raw_output: true if target_org
-      puts output
+      puts sf.project.generate_manifest from_org: target_org, output_dir: 'manifest', raw_output: true if target_org
 
       sf.project.retrieve_start manifest: 'manifest/package.xml', target_org: target_org, raw_output: true if retrieve_source
+      system 'code .' if editor
+    ensure
+      Dir.chdir base_dir
+    end
+
+    def self.update(project_name, params)
+      base_dir        = Dir.pwd
+      target_org      = params[:"target-org"]
+      editor          = params[:"editor-open"]
+
+      Dir.chdir project_name
+
+      manifest = 'manifest/package.xml'
+
+      FileUtils.rm manifest if FileTest.exist?(manifest)
+
+      puts sf.project.generate_manifest from_org: target_org, output_dir: 'manifest', raw_output: true
+      sf.project.retrieve_start manifest: 'manifest/package.xml', target_org: target_org, raw_output: true
       system 'code .' if editor
     ensure
       Dir.chdir base_dir
